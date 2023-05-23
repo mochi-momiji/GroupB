13d717988704fdd5a66abbf4ec9294da5597be2b
awatayuuto <yulovecat9@gmail.com>
2023-05-23 14:43:27 +0900

    スクリプトの追加
    左右の手の動きのスクリプト

---
+++ GroupB/Assets/HandMoveL.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMoveL : MonoBehaviour
+{
+    double timer = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.W)) 
+        {
+            handmove();
+        }
+    }
+
+    void handmove()
+    {
+        //経過時間のカウント
+        timer += Time.deltaTime;
+
+        //4秒間のループ
+        double t = timer % 4;
+
+        if (t < 1.0f || t > 3.0f)//2秒経過するまで下移動
+        {
+            transform.Translate(-0.01f, -0.01f, 0.0f);
+        }
+        else if (t < 3.0f)//4秒経過するまで上移動
+        {
+            transform.Translate(0.01f, 0.01f, 0.0f);
+        }
+    }
+}
